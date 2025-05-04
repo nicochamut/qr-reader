61931a3d0cb0cec53e04bc4ea51cc17eae34d207
Nicolas J. Chamut <nicochamut@gmail.com>
2025-05-04 18:25:32 -0300

    fix home scanner

--- src/components/QrScanner.jsx
+++ src/components/QrScanner.jsx
@@ -15,7 +15,7 @@ const ScannerWrapper = styled.div`
 
 const QrContainer = styled.div`
   width: 100%;
-  max-width: 400px;
+  max-width: 350px;
   width: 400px;
   border: 2px solid #4c98af;
   border-radius: 20px;
